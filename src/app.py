f8d24cf433f80fe4757540bf6e82fc7386fe3e50
Jaehyuk Kim <jaehyuk.kim.jk@gmail.com>
2024-11-14 14:20:42 +0900

    지역별 정비소 차트 추가

--- src/app.py
+++ src/app.py
@@ -12,6 +12,26 @@ import plotly.express as px
 
 import platform
 from matplotlib import font_manager, rc
+import pickle
+
+@st.cache_data
+def load_pickle():
+    with open("./data/charge_car_ratio.pkl" , "rb") as f:
+        charge_car_ratio  = pickle.load(f)
+
+    with open("./data/car_region.pkl" , "rb") as f:
+        car_region  = pickle.load(f)
+
+    with open("./data/car_charge.pkl" , "rb") as f:
+        car_charge  = pickle.load(f)
+
+    with open("./data/car_repairshop.pkl" , "rb") as f:
+        car_repairshop  = pickle.load(f)
+
+    with open("./data/result_car_charge.pkl" , "rb") as f:
+        result_car_charge  = pickle.load(f)
+
+    return charge_car_ratio, car_region, car_charge, car_repairshop, result_car_charge
 
 
 @st.cache_data
@@ -50,14 +70,18 @@ def main():
 
     elec_car_charger_df, elec_car_reg_df, elec_car_repair_df = load_data()
 
-    question = ['지역별 충전소', '지역별 전기차 등록대수','지역별 통합','brand 별 전기차 정비소 수','전기차 등록대수', '충전기당 전기차수']
+    charge_car_ratio, car_region, car_charge, car_repairshop, result_car_charge = load_pickle()
 
-    my_choice = st.selectbox('질문을 선택하시오', question)
+    print(charge_car_ratio.head(10))
+
+    question = ['지역별 충전소', '지역별 전기차 등록대수','지역별 차충기', '지역별 정비소','brand 별 전기차 정비소 수']
+
+    my_choice = st.selectbox('차트를 선택하시오', question)
 
     st.text( '{}대한 정보입니다.'.format(my_choice) )
 
     if my_choice == question[0] :
-        st.text('지역별 충전소 개수')
+        # st.text('지역별 충전소 개수')
 
         # 서울, 인천, 경기, 강원, 제주, 충청, 전라, 경상
 
@@ -70,7 +94,7 @@ def main():
         
     elif my_choice == question[1]:
 
-        st.text('지역별 전기차 등록대수')
+        # st.text('지역별 전기차 등록대수')
         elec_region_df = elec_car_reg_df.copy()
 
         elec_region_df[3] = elec_region_df[1]
@@ -119,33 +143,38 @@ def main():
         fig5, ax = plt.subplots()
         
         plt.xticks(rotation=90)  
-        plt.bar(elec_car_reg_region_df[3], elec_car_reg_region_df[2])        
+        plt.bar( elec_car_reg_region_df[3], elec_car_reg_region_df[2], color='green')        
         st.pyplot(fig5)
 
+    
     elif my_choice == question[2]:
-        st.text('brand 별 전기차 정비소 수')
-        print(elec_car_repair_df.head())
-        y_series = elec_car_repair_df.groupby(1).size()
-        y_series.sort_values(inplace=True)
-        print(y_series.head())
+        fig5, ax = plt.subplots()
 
-        print(y_series.info())
+        plt.title('지역별 차충기')
+        xs=charge_car_ratio['region'].to_list()
+        ys=charge_car_ratio['ratio'].to_list()
 
-        # fig3 = px.bar( y_series, x=0, y=1)    #plotly bar차트
-        # fig3.update_xaxes(title_text='brand')
-        # fig3.update_yaxes(title_text='count')
-        # st.plotly_chart(fig3)
-        # fig3 = plt.plot()
-        # plt.bar(y_series.index, y_series.values)
-        # st.pyplot(fig3)
+        plt.xlabel('Region')
+        plt.ylabel('Count')
+        
+        plt.bar(xs, ys, width=0.6, color='#ffc000')
+        st.pyplot(fig5)
 
-        fig3, ax = plt.subplots()
-        # plt.figure(figsize=(6,8))
-        plt.xticks(rotation=90)  
-        plt.bar(y_series.index, y_series.values)        
-        st.pyplot(fig3)
+        st.text('충전기 1대당 전기차 대수를 의미하는 것을 [차충비] 라고 한다.')
 
     elif my_choice == question[3]:
+        # st.text('brand 별 전기차 정비소 수')
+
+        print(car_repairshop.head())
+        fig6, ax = plt.subplots()
+        
+        plt.xticks(rotation=90)  
+        plt.bar(car_repairshop['지역'], car_repairshop['정비소'], color='cyan')        
+        st.pyplot(fig6)
+
+    elif my_choice == question[4]:
+        # st.text('brand 별 전기차 정비소 수')
+
         repair_df = elec_car_repair_df.groupby(1, as_index=False)[[3]].count()
         repair_df.sort_values([3], inplace=True)
         fig4, ax = plt.subplots()
@@ -153,18 +182,13 @@ def main():
         plt.xticks(rotation=90)  
         plt.bar(repair_df[1], repair_df[3])        
         st.pyplot(fig4)
-    elif my_choice == question[4]:
-        st.text('전기차 등록대수')
-        elec_car_reg_df.sort_values([2], inplace=True)
-        fig2 = px.bar( elec_car_reg_df, x=1, y=2)    #plotly bar차트
-        fig2.update_xaxes(title_text='지역')
-        fig2.update_yaxes(title_text='count')
-        st.plotly_chart(fig2)
-
-    elif my_choice == question[5]:
-        pass
-
-
+    # elif my_choice == question[4]:
+    #     st.text('전기차 등록대수')
+    #     elec_car_reg_df.sort_values([2], inplace=True)
+    #     fig2 = px.bar( elec_car_reg_df, x=1, y=2)    #plotly bar차트
+    #     fig2.update_xaxes(title_text='지역')
+    #     fig2.update_yaxes(title_text='count')
+    #     st.plotly_chart(fig2)
 
 
 
